b8cf8991bbdcea1b206bb410de27ad7190157a59
Daniel <catcrumpet@users.noreply.github.com>
2018-03-08 13:26:47 -0800

    Update collapse_epochs.R
    Updated to allow for any output epoch length. A hack job.

--- R/collapse_epochs.R
+++ R/collapse_epochs.R
@@ -2,17 +2,19 @@
 #'
 #' Collapse post-filtered activity counts into larger epoch "buckets".
 #' @param agdb A \code{tibble} (\code{tbl}) of activity data (at least) an \code{epochlength} attribute.
-#' @param epoch_len_out Output (longer) epoch length in seconds, must be exact multiple of the input epoch length. Currently only \code{epoch_len_out} = 60 is supported.
+#' @param epoch_len_out Output (longer) epoch length in seconds, must be exact multiple of the input epoch length.
 #' @param use_incomplete logical. Set to \code{TRUE} to follow ActiLife convention, which collapses all observed epochs even if they are incomplete.
-#' @return A \code{tibble} (\code{tbl}) of activity data collapsed into one-minute epochs.
+#' @return A \code{tibble} (\code{tbl}) of activity data collapsed into new epochs of specified length.
 #' @references ActiLife 6 User's Manual by the ActiGraph Software Department. 04/03/2012.
 #' @details
-#' Activity counts cannot be reintegrated into shorter epochs, e.g., 60s -> 10s. Currently, \code{collapse_epochs} integrates into 60s epochs only. This is not general but is sufficient for sleep analysis because the standard Sadeh and Cole-Kripke sleep algorithms were developed for 60s epoch data.
+#' Output epochs start at the first timestamp of the input data.
 #'
-#' Suppose we want to collapse from 15 to 60 seconds. A complete 60s epoch consists of four 15s epochs: 00, 15, 45 and 60. However, the first and last epochs would be incomplete if the device started/stopped collecting data mid-minute. ActiLife 6 uses these epochs anyway. For example, if only 45 and 60 are available for the first minute, then ActiLife will aggregate across these two epochs only. This is a reasonable approach to sleep analysis with the Sadeh and the Cole-Kripke algorithms which pad the beginning and the end of the time series with zeros anyway.
 #' @examples
 #' library("dplyr")
 #' data("gtxplus1day")
+#'
+#' gtxplus1day %>%
+#'   collapse_epochs(30)
 #'
 #' gtxplus1day %>%
 #'   collapse_epochs(60)
@@ -25,20 +27,16 @@ collapse_epochs <- function(agdb, epoch_len_out,
   collapse_factor <- epoch_len_out / attr(agdb, "epochlength")
   if (collapse_factor == 1) return(agdb)
 
-  # TODO: a more general approach to collapsing
-  # might use the findInterval function
-  # though care must be taken with "incomplete"
-  # epochs at the start/end of the time series
-
   agdb <- agdb %>%
-    do(collapse_epochs_(.data, collapse_factor, use_incomplete))
+    do(collapse_epochs_(.data, epoch_len_out, collapse_factor, use_incomplete))
 
   attr(agdb, "epochlength") <- epoch_len_out
   attr(agdb, "epochcount") <- nrow(agdb)
+  
   agdb
 }
 
-collapse_epochs_ <- function(data, collapse_factor, use_incomplete) {
+collapse_epochs_ <- function(data, epoch_len_out, collapse_factor, use_incomplete) {
 
   # Exclude lux which is summarised by `floor(mean(lux))`
   selected <- intersect(colnames(data),
@@ -48,7 +46,9 @@ collapse_epochs_ <- function(data, collapse_factor, use_incomplete) {
 
   data <- data %>%
     select_at(vars("timestamp", selected)) %>%
-    mutate(timestamp = floor_date(.data$timestamp, "mins"), n = 1L) %>%
+    mutate(timestamp = .data$timestamp[1] + 
+           seconds(floor(difftime(.data$timestamp, .data$timestamp[1], units = "secs") / epoch_len_out) * epoch_len_out)) %>%
+    mutate(n = 1L) %>%
     group_by(.data$timestamp) %>%
     summarise_all(sum)
 
@@ -56,5 +56,5 @@ collapse_epochs_ <- function(data, collapse_factor, use_incomplete) {
     data <- data %>% filter(.data$n == collapse_factor)
   }
 
-  data %>% select(- n)
+  data %>% select(-n)
 }
