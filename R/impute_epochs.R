2d6b49292d428a2284e5cf38179b61194d8f0814
Daniel <catcrumpet@users.noreply.github.com>
2018-09-15 13:21:10 -0700

    Update impute_epochs.R

--- R/impute_epochs.R
+++ R/impute_epochs.R
@@ -45,8 +45,7 @@ has_missing_epochs <- function(agdb) {
   if (anyNA(agdb$timestamp)) return(TRUE)
 
   epoch_len <- attr(agdb, "epochlength")
-  agdb %>% 
-    has_missing_epochs_(epoch_len))
+  has_missing_epochs_(agdb, epoch_len))
 }
 
 has_missing_epochs_ <- function(data, epoch_len) {
